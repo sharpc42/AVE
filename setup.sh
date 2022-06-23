c7bd33aff50dd563a5499d9d0f17efcde3572353
sharpc42 <chris.m.sharp@gmail.com>
2022-06-23 01:32:52 -0700

    Conda support & behavior fix

--- setup.sh
+++ setup.sh
@@ -23,62 +23,79 @@
 # check if already installed
 if test -f "./__installed__"
 then
-    echo "The AVE is already installed! :)"
+    printf "The AVE is already installed! :)\n"
     exit 0
 fi
 
-echo "\nThe AVE was tested for Python 3.9.5 - if you have problems, consider"
-echo "   updating to a version of Python at least as new as that.\n"
+printf "\nThe AVE was tested for Python 3.9.5 - if you have problems, consider\n"
+printf "   updating to a version of Python at least as new as that.\n"
 
 # install required packages
-echo "I'd like to check for and automatically install required packages now,"
-echo "   including Athena++ the simulation code AVE wraps. Is that okay? (y/n)"
-
-read PRE_REQS
-CHK_REQ=false
-
-while [ $CHK_REQ == 'false' ]
+CHK_REQ='false'
+while [ $CHK_REQ = 'false' ]
 do
-    if [[ $PRE_REQS == 'y' ]]
+    printf "\nI'd like to check for and automatically install required packages now,\n"
+    printf "   including Athena++ the simulation code AVE wraps. Is that okay? (y/n)"
+    read PRE_REQS
+
+    if [ $PRE_REQS = 'y' ]
       then
-        CHK_REQ=true
-        echo "\nGot it. Thank you. Checking for and installing them...\n"
-        pip install -r requirements.txt
-        echo "Done.\n"
+	CHK_REQ='true'
+        CHK_REQ_2='false'
+	while [ $CHK_REQ_2 = 'false' ]
+	do
+		printf "\nAre you using an Anaconda or Miniconda environment? (y/n)"
+		read CONDA
+		if [ $CONDA = 'y' ]
+		    then
+			CHK_REQ_2='true'
+	        	printf "\nGot it. Thank you. Updating dependencies...\n"
+			conda config --append channels conda-forge
+			conda install --file requirements
+		elif [ $CONDA = 'n' ]
+		    then
+			CHK_REQ_2='true'
+			printf "\nGot it. Thank you. Updating dependencies...\n"
+        		pip install -r requirements
+		else
+			printf "\nPlease answer yes or no. Thank you.\n"
+		fi
+        	printf "Done.\n"
+	done
         if [ -d "athena" ]
           then
-            echo "*Athena++ already installed*"
+            printf "\n*Athena++ already installed*"
         else
-            echo "Now I will check for and install Athena++ as necessary.\nPress any key to continue."
+            printf "\nNow I will check for and install Athena++ as necessary.\nPress any key to continue."
             read -n 1
-            echo "Now installing Athena++ code...\n"
+            printf "\nNow installing Athena++ code...\n"
             git clone https://github.com/PrincetonUniversity/athena/
-            echo "\nAthena++ is installed.\n"
-            echo "" > athena/src/pgen/dummy.cpp
+            printf "\nAthena++ is installed.\n"
+            printf "" > athena/src/pgen/dummy.cpp
             cd athena
             ./configure.py --prob dummy
             make
             cd ..
             rm athena/src/pgen/dummy.cpp
         fi
-    elif [[ $PRE_REQS == 'n' ]]
+    elif [ $PRE_REQS = 'n' ]
       then
-        CHK_REQ=true
-        echo "\nOkay. If you encounter any problems running AVE, you might want"
-        echo "   to double-check with the documentation to make sure you have all"
-        echo "   the right Python packages. You'll also need to download Athena++"
-        echo "   manually.\n"
+        CHK_REQ='true'
+        printf "\nOkay. If you encounter any problems running AVE, you might want\n"
+        printf "   to double-check with the documentation to make sure you have all\n"
+        printf "   the right Python packages. You'll also need to download Athena++\n"
+        printf "   manually.\n"
     else
-        echo "\nPlease answer yes or no. Thank you.\n"
+        printf "\nPlease answer yes or no. Thank you.\n"
     fi
-    rm requirements.txt
+    rm requirements
 done
 
 chmod +x ave.py
 
-echo "AVE file can be run from the terminal.\n"
+printf "AVE file can be run from the terminal.\n"
 
 echo "" > __installed__
 
-echo "You're all set to start using the AVE!\n"
+printf "You're all set to start using the AVE!\n"
 exit 0
